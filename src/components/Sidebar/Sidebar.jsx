b442901271fe41059c388d726479120f58073f58
Aiko14510 <aasthasherchan78699@gmail.com>
2025-06-02 00:00:43 +0545

    Added the API key to search dynamic data which show all the video in home page

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -15,43 +15,70 @@ import tom from "../../assets/tom.png";
 import megan from "../../assets/megan.png";
 import cameron from "../../assets/cameron.png";
 
-const Sidebar = ({sidebar}) => {
+const Sidebar = ({sidebar, category, setCategory}) => {
   return (
-    <div className={`sidebar ${sidebar?"":"small-sidebar"}`}>
+    <div className={`sidebar ${sidebar ? "" : "small-sidebar"}`}>
       <div className="sortcut-links">
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 0 ? "active" : ""}`}
+          onClick={() => setCategory(0)}
+        >
           <img src={home} alt="Home icon" />
           <p>Home</p>
         </div>
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 20 ? "active" : ""}`}
+          onClick={() => setCategory(20)}
+        >
           <img src={game_icon} alt="Home icon" />
           <p>Game</p>
         </div>
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 2 ? "active" : ""}`}
+          onClick={() => setCategory(2)}
+        >
           <img src={automobiles} alt="Home icon" />
           <p>Automobiles</p>
         </div>
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 17 ? "active" : ""}`}
+          onClick={() => setCategory(17)}
+        >
           <img src={sports} alt="Home icon" />
           <p>Sports</p>
         </div>
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 24 ? "active" : ""}`}
+          onClick={() => setCategory(24)}
+        >
           <img src={entertainment} alt="Home icon" />
           <p>Entertainent</p>
         </div>
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 28 ? "active" : ""}`}
+          onClick={() => setCategory(28)}
+        >
           <img src={tech} alt="Home icon" />
           <p>Technology</p>
         </div>
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 10 ? "active" : ""}`}
+          onClick={() => setCategory(10)}
+        >
           <img src={music} alt="Home icon" />
           <p>Music</p>
         </div>
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 22 ? "active" : ""}`}
+          onClick={() => setCategory(22)}
+        >
           <img src={blogs} alt="Home icon" />
           <p>Blogs</p>
         </div>
-        <div className="side-link">
+        <div
+          className={`side-link ${category === 25 ? "active" : ""}`}
+          onClick={() => setCategory(25)}
+        >
           <img src={news} alt="Home icon" />
           <p>News</p>
         </div>
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const API_KEY = "AIzaSyCzj33z4O22a-lmRXeR3O9rXcayEuPoDxE";
+
+export const value_converter = (value) => {
+    if(value >= 1000000) {
+        return (value / 1000000).toFixed(1) + "M";
+    }
+    else if(value >= 1000) {
+        return (value / 1000).toFixed(1) + "K";
+    }
+    else {
+        return value;
+    }
+
+}
